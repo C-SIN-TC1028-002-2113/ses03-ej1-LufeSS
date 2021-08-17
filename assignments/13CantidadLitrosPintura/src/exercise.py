52e9c51ba86e7fdf62656a40931515249ad2d89f
LufeSS <A01742322@itesm.mx>
2021-08-17 14:32:21 +0000

    PEMDAS y Pintura

--- assignments/13CantidadLitrosPintura/src/exercise.py
+++ assignments/13CantidadLitrosPintura/src/exercise.py
@@ -1,10 +1,11 @@
+import math
 def main():
     #escribe tu código abajo de esta línea
-    pass
-    areaPintar = float(input("Area a pintar en metros: "))
-    rendimiento = float(input("Rendimiento (m2/1): "))
-    litrosComprar = int(areaPintar/rendimiento)
-    print("Litros a comprar:",litrosComprar)
+    area = float(input("Area a pintar en metros: "))
+    rendimiento = float(input("Rendimiento (m2/l): "))
+
+    litros = math.ceil(area/rendimiento)
+    print("Litros a comprar:",litros)
 
 
 
