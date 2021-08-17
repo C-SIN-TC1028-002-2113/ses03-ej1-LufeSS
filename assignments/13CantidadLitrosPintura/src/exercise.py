182faeb07f1da77a3937ea8cfb7be14e7a8d3b27
LufeSS <A01742322@itesm.mx>
2021-08-17 14:11:00 +0000

    PEMDAS y Litros de pintura

--- assignments/13CantidadLitrosPintura/src/exercise.py
+++ assignments/13CantidadLitrosPintura/src/exercise.py
@@ -1,6 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
+    areaPintar = float(input("Area a pintar en metros: "))
+    rendimiento = float(input("Rendimiento (m2/1) "))
+    litrosComprar = int(areaPintar/rendimiento)
+    print("Litros a comprar:",litrosComprar)
+
+
+
+
+
 
 if __name__ == '__main__':
     main()
